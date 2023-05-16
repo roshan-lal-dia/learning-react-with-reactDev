972d772da626de390ea99dab4f7ccc82954c3de1
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-05-16 14:40:29 +0000

    learned jsx escape

--- src/App.js
+++ src/App.js
@@ -8,10 +8,35 @@ function MyButton() {
   );
 }
 
+function Profile() {
+  const USER = {
+    name: "Roshan Lal Dia 💞",
+    imageUrl:
+      "https://www.tensionends.com/wp-content/uploads/2022/09/Beautiful-Girl-DP-Images-1.jpg",
+    imageSize: 90,
+  };
+  return (
+    <>
+      <h1>{USER.name}</h1>
+      <img
+        className="avatar"
+        src={USER.imageURL}
+        alt={"Photo of " + USER.name}
+        style={{
+          width: USER.imageSize,
+          height: USER.imageSize,
+        }}
+      />
+      <br />
+    </>
+  );
+}
+
 export default function MyApp() {
   return (
     <>
       <h2>Hello World</h2>
+      <Profile />
       <MyButton />
     </>
   );

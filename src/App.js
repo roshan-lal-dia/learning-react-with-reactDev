4d0087f65356473119da115a430a50cd8393e366
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-05-16 14:15:30 +0000

    addes first example

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function MyButton() {
+  return(
+    <div>
+      <button>This is a Button</button>
+    </div>
+  );
+}
+
+export default function MyApp(){
+  return(
+    <div>
+      <h2>Hello World</h2>
+      <MyButton />
+    </div>
+  );
+}
